933f48f1c48e38f4eb186087b70565ba016252b0
Senitha-EminentB <senitha200109@gmail.com>
2026-08-28 14:48:57 +0530

    fix: add nodejs22 runtime and auth proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
+      '/auth': 'https://project-62f13b8e-684a-4407-bb9.uc.r.appspot.com/',
       '/products': 'https://project-62f13b8e-684a-4407-bb9.uc.r.appspot.com/',
       '/categories': 'https://project-62f13b8e-684a-4407-bb9.uc.r.appspot.com/',
       '/orders': 'https://project-62f13b8e-684a-4407-bb9.uc.r.appspot.com/',
       '/uploads': 'https://project-62f13b8e-684a-4407-bb9.uc.r.appspot.com/',
     }
+
   }
 })
